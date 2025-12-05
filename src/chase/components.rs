05f829ea8f248c8d4815cfec05203b81bff388ee
Payne325 <alex.payne325@gmail.com>
2025-12-05 21:57:56 +0000

    bundo-li-os

--- src/chase/components.rs
+++ src/chase/components.rs
@@ -1,7 +1,49 @@
 use bevy::prelude::*;
 
 #[derive(Component)]
-pub struct Elf {}
+pub(super) struct Elf {}
+
+#[derive(Bundle)]
+pub(super) struct ElfBundle {
+    elf: Elf,
+    transform: Transform,
+    sprite: Sprite,
+    //RigidBody::Dynamic,
+    //add collider here
+}
+
+impl Elf {
+    pub(super) fn new_bundle(asset_server: &Res<AssetServer>) -> ElfBundle {
+        ElfBundle {
+            elf: Elf {},
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            sprite: Sprite::from_image(asset_server.load("sprites/elf.png")),
+            //RigidBody::Dynamic,
+            //add collider here
+        }
+    }
+}
 
 #[derive(Component)]
-pub struct Dog {}
+pub(super) struct Dog {}
+
+#[derive(Bundle)]
+pub(super) struct DogBundle {
+    dog: Dog,
+    transform: Transform,
+    sprite: Sprite,
+    //RigidBody::Dynamic,
+    //add collider here
+}
+
+impl Dog {
+    pub(super) fn new_bundle(asset_server: &Res<AssetServer>) -> DogBundle {
+        DogBundle {
+            dog: Dog {},
+            transform: Transform::from_xyz(-400.0, 400.0, 0.0),
+            sprite: Sprite::from_image(asset_server.load("sprites/zeus_1.png")),
+            //RigidBody::Dynamic,
+            //add collider here
+        }
+    }
+}
--- src/chase/observers.rs
+++ src/chase/observers.rs
@@ -14,19 +14,8 @@ pub(super) fn observe_game_start(
     state: Res<State<ChaseGameState>>,
     next_state: ResMut<NextState<ChaseGameState>>,
 ) {
-    commands.spawn((
-        Elf {},
-        Transform::from_xyz(0.0, 0.0, 0.0),
-        Sprite::from_image(asset_server.load("sprites/elf.png")),
-        //RigidBody::Dynamic,
-        //add collider here
-    ));
-
-    commands.spawn((
-        Dog {},
-        Transform::from_xyz(-400.0, 400.0, 0.0),
-        Sprite::from_image(asset_server.load("sprites/zeus_1.png")),
-    ));
+    commands.spawn(Elf::new_bundle(&asset_server));
+    commands.spawn(Dog::new_bundle(&asset_server));
 
     println!("BADABING!");
     toggle_game_state(state, next_state);
